9aa72889482a8034273ca41553de516e81395290
supersakana <zacwilliamson@icloud.com>
2025-02-10 21:01:20 -0600

    small updates for deployments

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ This code is to create a proveably random smart contract lottery.
 3. Chainlink VRF should generate a provably random number.
 
 4. Chainlink Automation should trigger the lottery draw regularly.
+
+https://sepolia.etherscan.io/address/0xa085FD6A5241C1a9D8F5f7ACF12088452E50f078#code
--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {Raffle} from "../../src/Raffle.sol";
-import {DeployRaffle} from "../../script/DeployRaffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {Vm} from "forge-std/Vm.sol";
 // import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
